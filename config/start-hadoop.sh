6ed2b854c6671df89ed25f23a88073dd5e9965a2
mustafa <fanghailiang2016@gmail.com>
2020-07-24 23:58:33 +0800

    test

--- config/start-hadoop.sh
+++ config/start-hadoop.sh
@@ -6,6 +6,10 @@ $HADOOP_HOME/sbin/start-dfs.sh
 
 echo -e "\n"
 
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver 
+
+echo -e "\n"
+
 $HADOOP_HOME/sbin/start-yarn.sh
 
 echo -e "\n"
--- start-container.sh
+++ start-container.sh
@@ -3,6 +3,7 @@
 # the default node number is 3
 N=${1:-3}
 
+sudo mkdir -p /data/scaladata/master/datas
 
 # start hadoop master container
 sudo docker rm -f hadoop-master &> /dev/null
@@ -11,6 +12,12 @@ sudo docker run -itd \
                 --net=hadoop \
                 -p 50070:50070 \
                 -p 8088:8088 \
+                -p 9000:9000 \
+                -p 8032:8032 \
+                -p 19888:19888 \
+                -v /data/scaladata/master/datas/tmp:/root/hdfs/tmp \
+                -v /data/scaladata/master/datas/namenode:/root/hdfs/namenode \
+                -v /data/scaladata/master/datas/datanode:/root/hdfs/datanode \
                 --name hadoop-master \
                 --hostname hadoop-master \
                 kiwenlau/hadoop:1.0 &> /dev/null
@@ -20,10 +27,14 @@ sudo docker run -itd \
 i=1
 while [ $i -lt $N ]
 do
+    sudo mkdir -p /data/scaladata/slave$i/datas
 	sudo docker rm -f hadoop-slave$i &> /dev/null
 	echo "start hadoop-slave$i container..."
 	sudo docker run -itd \
 	                --net=hadoop \
+                    -v /data/scaladata/slave$i/datas/tmp:/root/hdfs/tmp \
+                    -v /data/scaladata/slave$i/datas/namenode:/root/hdfs/namenode \
+                    -v /data/scaladata/slave$i/datas/datanode:/root/hdfs/datanode \
 	                --name hadoop-slave$i \
 	                --hostname hadoop-slave$i \
 	                kiwenlau/hadoop:1.0 &> /dev/null
